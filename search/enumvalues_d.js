8735edd43cd5a7b855b32617c572ada45dd8dd04
Entwicklungsburo-Stresing <florian.hahn@stresing.de>
2023-11-16 14:32:19 +0000

    Update from https://github.com/Entwicklungsburo-Stresing/EBST_CAM/commit/5d28d8eebaca52960324d1e8990e6360e446f2e1

--- search/enumvalues_d.js
+++ search/enumvalues_d.js
@@ -2,5 +2,8 @@ var searchData=
 [
   ['xck_0',['xck',['../enum__settings_8h.html#a6d33f21be3f0c5cf91c5d638e8aca086a7a03b9103736c00a1b574c46bc8e8e31',1,'enum_settings.h']]],
   ['xckmsb_5fbit_5fstimer_5fon_1',['XCKMSB_bit_stimer_on',['../enum__hardware_8h.html#a7edba2515104bc9df9622c38d05b5351a18e5d9ae516eb2754d1bb18552b3afee',1,'enum_hardware.h']]],
-  ['xckmsb_5fbitindex_5fstimer_5fon_2',['XCKMSB_bitindex_stimer_on',['../enum__hardware_8h.html#a7edba2515104bc9df9622c38d05b5351a2a22b106e55b453b0192f74294e52cdc',1,'enum_hardware.h']]]
+  ['xckmsb_5fbitindex_5ff_5fext_5ftrig_2',['XCKMSB_bitindex_F_EXT_TRIG',['../enum__hardware_8h.html#a7edba2515104bc9df9622c38d05b5351aa4a160cd6ccfa8fd57d29285529be978',1,'enum_hardware.h']]],
+  ['xckmsb_5fbitindex_5freset_5fms_3',['XCKMSB_bitindex_reset_ms',['../enum__hardware_8h.html#a7edba2515104bc9df9622c38d05b5351a10f84c349b5965acb57e45fa3f789289',1,'enum_hardware.h']]],
+  ['xckmsb_5fbitindex_5freset_5fns_4',['XCKMSB_bitindex_reset_ns',['../enum__hardware_8h.html#a7edba2515104bc9df9622c38d05b5351a82419149bee66353fe2fddd8d59e525c',1,'enum_hardware.h']]],
+  ['xckmsb_5fbitindex_5fstimer_5fon_5',['XCKMSB_bitindex_stimer_on',['../enum__hardware_8h.html#a7edba2515104bc9df9622c38d05b5351a2a22b106e55b453b0192f74294e52cdc',1,'enum_hardware.h']]]
 ];
