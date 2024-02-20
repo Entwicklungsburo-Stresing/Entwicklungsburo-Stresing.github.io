022ae173192ab13ee23c91f3c2db7e283b516762
Entwicklungsburo-Stresing <florian.hahn@stresing.de>
2024-02-20 11:09:17 +0000

    Update from https://github.com/Entwicklungsburo-Stresing/EBST_CAM/commit/eea4a19b8098e6cab05e59c7c2e09df6b357b8b2

--- search/all_12.js
+++ search/all_12.js
@@ -11,6 +11,5 @@ var searchData=
   ['userbufferendptr_8',['userBufferEndPtr',['../globals_8h.html#a20eca6d7ab5733c19af8ffa35af26b87',1,'userBufferEndPtr:&#160;globals.c'],['../globals_8c.html#a20eca6d7ab5733c19af8ffa35af26b87',1,'userBufferEndPtr:&#160;globals.c']]],
   ['userbufferwritepos_9',['userBufferWritePos',['../globals_8h.html#a4daec5110a9834b66580a51f21994ff2',1,'userBufferWritePos:&#160;globals.c'],['../globals_8c.html#a4daec5110a9834b66580a51f21994ff2',1,'userBufferWritePos:&#160;globals.c']]],
   ['userbufferwritepos_5flast_10',['userBufferWritePos_last',['../globals_8h.html#a17949b41b5a2c341b572650ce237006c',1,'userBufferWritePos_last:&#160;globals.c'],['../globals_8c.html#a17949b41b5a2c341b572650ce237006c',1,'userBufferWritePos_last:&#160;globals.c']]],
-  ['useswtrig_11',['useSWTrig',['../structglobal__vars.html#a5aebfdc96b3574ead6386c63ac26a3e1',1,'global_vars::useSWTrig'],['../globals_8h.html#a4bf1fd7a1bcd68978b3da5e2f96aa770',1,'useSWTrig:&#160;globals.c'],['../globals_8c.html#a4bf1fd7a1bcd68978b3da5e2f96aa770',1,'useSWTrig:&#160;globals.c']]],
-  ['useswtrig_5ftemp_12',['useSWTrig_temp',['../globals_8c.html#ad29b8876e23f2e7106411b493da7ffe0',1,'globals.c']]]
+  ['useswtrig_11',['useSWTrig',['../structglobal__vars.html#a5aebfdc96b3574ead6386c63ac26a3e1',1,'global_vars']]]
 ];
