e8399ae80ad1258b2b85ea7fecffc9befea5d88a
Entwicklungsburo-Stresing <florian.hahn@stresing.de>
2024-04-29 14:44:16 +0000

    Update from https://github.com/Entwicklungsburo-Stresing/EBST_CAM/commit/03dac5c291e39d4a4e5c5d86815af218e52965f8

--- search/enums_a.js
+++ search/enums_a.js
@@ -4,9 +4,10 @@ var searchData=
   ['scanindex_5fbits_5ft_1',['ScanIndex_bits_t',['../enum__hardware_8h.html#a15896ca263195923b15606bb739b92b5',1,'enum_hardware.h']]],
   ['sdat_5fbits_5ft_2',['SDAT_bits_t',['../enum__hardware_8h.html#aeb7b45efaf34feb37817a80853e4e407',1,'enum_hardware.h']]],
   ['sensor_5ftype_5ft_3',['sensor_type_t',['../enum__settings_8h.html#a089f166159fb19f10d81c65c1d8793a2',1,'enum_settings.h']]],
-  ['special_5flast_5fpixels_5ft_4',['special_last_pixels_t',['../enum__hardware_8h.html#aa5345f9ae48237dce9cbde9e67b4a3aa',1,'enum_hardware.h']]],
-  ['special_5fpixels_5fenum_5ft_5',['special_pixels_enum_t',['../enum__hardware_8h.html#ab4e169cedf37f6a55251e0d15f33a2b1',1,'enum_hardware.h']]],
-  ['split_5fmode_5ft_6',['split_mode_t',['../enum__settings_8h.html#a2f4a284b0a5ab5d474b897488289158c',1,'enum_settings.h']]],
-  ['sslope_5ft_7',['sslope_t',['../enum__settings_8h.html#a807bb0212417bc8c5b431aad2fbc2f00',1,'enum_settings.h']]],
-  ['sti_5fmode_5ft_8',['sti_mode_t',['../enum__settings_8h.html#a0bdcebe838e77385002ccc4077a6e743',1,'enum_settings.h']]]
+  ['settings_5flevel_5ft_4',['settings_level_t',['../enum__settings_8h.html#ac4e3d5d9199c915edd34fcf8c0be1166',1,'enum_settings.h']]],
+  ['special_5flast_5fpixels_5ft_5',['special_last_pixels_t',['../enum__hardware_8h.html#aa5345f9ae48237dce9cbde9e67b4a3aa',1,'enum_hardware.h']]],
+  ['special_5fpixels_5fenum_5ft_6',['special_pixels_enum_t',['../enum__hardware_8h.html#ab4e169cedf37f6a55251e0d15f33a2b1',1,'enum_hardware.h']]],
+  ['split_5fmode_5ft_7',['split_mode_t',['../enum__settings_8h.html#a2f4a284b0a5ab5d474b897488289158c',1,'enum_settings.h']]],
+  ['sslope_5ft_8',['sslope_t',['../enum__settings_8h.html#a807bb0212417bc8c5b431aad2fbc2f00',1,'enum_settings.h']]],
+  ['sti_5fmode_5ft_9',['sti_mode_t',['../enum__settings_8h.html#a0bdcebe838e77385002ccc4077a6e743',1,'enum_settings.h']]]
 ];
