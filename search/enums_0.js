f6d045d9597c493dc7314bf068d0c8e0d6016ae0
Entwicklungsburo-Stresing <florian.hahn@stresing.de>
2024-07-18 14:16:42 +0000

    Update from https://github.com/Entwicklungsburo-Stresing/EBST_CAM/commit/a3ad95ab4ed575d6f5c7847be20bdacbb25d1592

--- search/enums_0.js
+++ search/enums_0.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['adc_5fmode_5ft_0',['adc_mode_t',['../enum__settings_8h.html#a760a8dafdcddcfadfea0aa3e01159164',1,'enum_settings.h']]],
   ['arreg_5fbits_5ft_1',['ARREG_bits_t',['../enum__hardware_8h.html#aed7f564a2634450efefa7cd7d1dfaaf2',1,'enum_hardware.h']]],
-  ['autotune_5fchannel_5franges_5ft_2',['autotune_channel_ranges_t',['../enum__hardware_8h.html#a74388ebd004c60c71facf4217301562b',1,'enum_hardware.h']]]
+  ['autotune_5fchannel_5franges_5fhsir_5ft_2',['autotune_channel_ranges_hsir_t',['../enum__hardware_8h.html#a8b853f5fb8845767f82de96ee25f2d59',1,'enum_hardware.h']]],
+  ['autotune_5fchannel_5franges_5fhsvis_5ft_3',['autotune_channel_ranges_hsvis_t',['../enum__hardware_8h.html#aea22779edbaf246383c1ae9d61f9211a',1,'enum_hardware.h']]]
 ];
