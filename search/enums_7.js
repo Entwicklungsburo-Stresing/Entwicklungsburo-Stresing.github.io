2216704d4d16d0b051f13ed2de6882ce2360f68e
Entwicklungsburo-Stresing <florian.hahn@stresing.de>
2025-04-02 10:40:30 +0000

    Update from https://github.com/Entwicklungsburo-Stresing/EBST_CAM/commit/1db1079cb9f85493fef7a92fc94dcea9bbc35527

--- search/enums_7.js
+++ search/enums_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['ioctrl_5fregister_5faddress_5ft_0',['ioctrl_register_address_t',['../enum__hardware_8h.html#a57dd656066257660e8a347df5680e80f',1,'enum_hardware.h']]],
-  ['irqreg_5fbits_5ft_1',['IRQREG_bits_t',['../enum__hardware_8h.html#a112bb7cac94f9939207725ea4c525e5c',1,'enum_hardware.h']]]
+  ['ioctrl_5fshutter_5ft_1',['ioctrl_shutter_t',['../enum__hardware_8h.html#a69862b42ea46c07a392509caabbc67b8',1,'enum_hardware.h']]],
+  ['irqreg_5fbits_5ft_2',['IRQREG_bits_t',['../enum__hardware_8h.html#a112bb7cac94f9939207725ea4c525e5c',1,'enum_hardware.h']]]
 ];
