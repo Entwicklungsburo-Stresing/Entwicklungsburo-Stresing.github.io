8c945552a0ea98e7430f3eae7234903fa82a8d29
Entwicklungsburo-Stresing <florian.hahn@stresing.de>
2025-04-03 10:47:37 +0000

    Update from https://github.com/Entwicklungsburo-Stresing/EBST_CAM/commit/d54fdf0c69f30919c854228c1b684c4426685ee4

--- search/enums_3.js
+++ search/enums_3.js
@@ -2,9 +2,10 @@ var searchData=
 [
   ['dac8568_5flocation_5ft_0',['DAC8568_location_t',['../enum__settings_8h.html#a09974326d8346ab51cc2e499aabc55e8',1,'enum_settings.h']]],
   ['dac_5fregister_5faddresses_5ft_1',['dac_register_addresses_t',['../enum__hardware_8h.html#a4f248f174f6a6ad94a112e607b78fd57',1,'enum_hardware.h']]],
-  ['devicecontrol_5fbits_5ft_2',['DeviceControl_bits_t',['../enum__hardware_8h.html#a27c45d119d78ca12b7e38e47cb02907f',1,'enum_hardware.h']]],
-  ['dma_5faddresses_5ft_3',['dma_addresses_t',['../enum__hardware_8h.html#a8e76e2a560aa415ee8df741caecff6ad',1,'enum_hardware.h']]],
-  ['dmabufsizeinscans_5fbits_5ft_4',['DmaBufSizeInScans_bits_t',['../enum__hardware_8h.html#aab664f914a04fe4ff8bd3310b852deb7',1,'enum_hardware.h']]],
-  ['dmasperintr_5fbits_5ft_5',['DMAsPerIntr_bits_t',['../enum__hardware_8h.html#acc38a9e04cce5562fedf5864fa1acf4b',1,'enum_hardware.h']]],
-  ['dscctrl_5fbits_5ft_6',['DSCCtrl_bits_t',['../enum__hardware_8h.html#abcb8c0b3316bf062919b5d1a2673004c',1,'enum_hardware.h']]]
+  ['dbr_5fbits_5ft_2',['DBR_bits_t',['../enum__hardware_8h.html#a16cf9b02f1b23e1bef5b69f90e4f1534',1,'enum_hardware.h']]],
+  ['devicecontrol_5fbits_5ft_3',['DeviceControl_bits_t',['../enum__hardware_8h.html#a27c45d119d78ca12b7e38e47cb02907f',1,'enum_hardware.h']]],
+  ['dma_5faddresses_5ft_4',['dma_addresses_t',['../enum__hardware_8h.html#a8e76e2a560aa415ee8df741caecff6ad',1,'enum_hardware.h']]],
+  ['dmabufsizeinscans_5fbits_5ft_5',['DmaBufSizeInScans_bits_t',['../enum__hardware_8h.html#aab664f914a04fe4ff8bd3310b852deb7',1,'enum_hardware.h']]],
+  ['dmasperintr_5fbits_5ft_6',['DMAsPerIntr_bits_t',['../enum__hardware_8h.html#acc38a9e04cce5562fedf5864fa1acf4b',1,'enum_hardware.h']]],
+  ['dscctrl_5fbits_5ft_7',['DSCCtrl_bits_t',['../enum__hardware_8h.html#abcb8c0b3316bf062919b5d1a2673004c',1,'enum_hardware.h']]]
 ];
